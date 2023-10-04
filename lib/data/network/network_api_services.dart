16379a67650e3bfd35c0cf0bee58996a21fe0001
tohin <tohin214@gmail.com>
2023-10-04 21:08:50 +0600

    api response and response exception handling added

---
+++ lib/data/network/network_api_services.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_mvvm_example/data/network/base_api_services.dart';
+import 'package:flutter_mvvm_example/data/response/app_exceptions.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkApiServices extends BaseApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response =
+          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      final response =
+          await http.post(Uri.parse(url),body: data).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 404:
+        throw UnauthorizedException(response.body.toString());
+      default:
+        throw FetchDataException(
+            'Error occurred while communicating with server' +
+                'with status code' +
+                response.statusCode.toString());
+    }
+  }
+}
--- lib/data/response/app_exceptions.dart
+++ lib/data/response/app_exceptions.dart
@@ -14,6 +14,7 @@ class AppException implements Exception {
 
 
 class FetchDataException extends AppException{
+
   FetchDataException([String? message]) : super(message,'Error During Communication');
 }
 
