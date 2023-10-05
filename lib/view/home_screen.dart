0b7cf8bc2e9e98766f6b825e93c465ff16ac3d25
toh1n <tohin214@gmail.com>
2023-10-06 02:02:26 +0600

    toast and flushbar added

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_example/utils/routes/routes_name.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,9 +14,13 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const Text("Home Screen"),
-            ElevatedButton(onPressed: (){}, child: const Text("click"))
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, RoutesName.login);
+            }, child: const Text("click"))
           ],
         ),
       ),
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_example/utils/routes/routes_name.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,9 +14,13 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text("Home Screen"),
-            ElevatedButton(onPressed: (){}, child: const Text("click"))
+            const Text("Login Screen"),
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, RoutesName.home);
+            }, child: const Text("click"))
           ],
         ),
       ),
